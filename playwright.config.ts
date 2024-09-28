776fd4359051fd51b3022a74893acf73ae770413
Pedro Lima e Silva <pedrocaslsilva403@gmail.com>
2024-09-28 23:02:33 +0100

    Add responsiveness tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,37 @@
-import type { PlaywrightTestConfig } from '@playwright/test';
+import { devices, type PlaywrightTestConfig } from 'playwright/test';
 
 const config: PlaywrightTestConfig = {
+	expect: {
+		toHaveScreenshot: { maxDiffPixels: 0 }
+	},
+  projects: [
+    {
+      name: 'chromium',
+      use: {
+        ...devices['Desktop Chrome'],
+      },
+    },
+    {
+      name: 'Mobile Safari Small',
+      use: {
+        ...devices['iPhone 13'],
+      },
+    },
+    {
+      name: 'Mobile Safari',
+      use: {
+        ...devices['iPhone 15 Pro Max'],
+      },
+    },
+
+		{
+      name: 'Mobile Landscape',
+      use: {
+        ...devices['iPhone 15 Pro Max landscape'],
+      },
+    },
+  ],
+	updateSnapshots: 'missing',
 	webServer: {
 		command: 'npm run build && npm run preview',
 		port: 4173
